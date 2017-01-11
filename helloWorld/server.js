228eda9ae95d16f7c0d4e17f76dabbee19d2ed54
tejaswigowda <tejaswi@foxyninjastudios.com>
2017-01-11 09:45:29 -0700

    ac

--- helloWorld/server.js
+++ helloWorld/server.js
@@ -1,10 +1,10 @@
 var http = require("http");
 
-var callback = function (req, res) { // req -> request object; res -> response object
+function callback (req, res) { // req -> request object; res -> response object
    res.writeHead(200, {'Content-Type': 'text/plain'}); // send response header
    res.end("hello world"); // send response body
 }
 
 var server = http.createServer(callback) // create an http server
-server.listen(1234, "127.0.0.1"); // make server listen to port 1337
+server.listen(1234, "127.0.0.1"); // make server listen to port 1234
 console.log("Server running at: "+ "http://127.0.0.1:1234");
