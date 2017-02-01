b2bf48e5fa3facb47f0133fdf0e609c473f75904
tejaswigowda <tejaswi@foxyninjastudios.com>
2017-02-01 09:36:44 -0700

    ac

---
+++ simpleMathServer/server.js
@@ -0,0 +1,26 @@
+var express = require("express");
+var app = express();
+var bodyParser = require('body-parser');
+var errorHandler = require('errorhandler');
+var methodOverride = require('method-override');
+var hostname = process.env.HOSTNAME || 'localhost';
+var port = 8080;
+
+app.get("/", function (req, res) {
+      res.redirect("/index.html");
+});
+
+app.get("/eval", function (req, res) {
+    console.log(req.query);
+    var result  = 0
+
+    res.send(result.toString()); // send response body
+});
+
+app.use(methodOverride());
+app.use(bodyParser());
+app.use(express.static(__dirname + '/public'));
+app.use(errorHandler());
+
+console.log("Simple static server listening at http://" + hostname + ":" + port);
+app.listen(port);
