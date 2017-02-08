eb844844c00caa3b857ad9a2e95910a694866871
tejaswigowda <tejaswi@foxyninjastudios.com>
2017-02-08 09:36:38 -0700

    in class

---
+++ todoExample/server.js
@@ -0,0 +1,64 @@
+var express = require("express");
+var app = express();
+var bodyParser = require('body-parser');
+var errorHandler = require('errorhandler');
+var methodOverride = require('method-override');
+var hostname = process.env.HOSTNAME || 'localhost';
+var port = 8080;
+
+app.get("/", function (req, res) {
+      res.redirect("/index.html");
+});
+
+
+var addCallback = function (req, res) {
+    var a = parseFloat(req.query.a);
+    var b = parseFloat(req.query.b);
+    var result  = a + b;
+
+    res.send(result.toString()); // send response body
+}
+
+
+var subCallback = function (req, res) {
+    var a = parseFloat(req.query.a);
+    var b = parseFloat(req.query.b);
+    var result  = a - b;
+
+    res.send(result.toString()); // send response body
+}
+
+
+var multCallback = function (req, res) {
+    var a = parseFloat(req.query.a);
+    var b = parseFloat(req.query.b);
+    var result  = a * b;
+
+    res.send(result.toString()); // send response body
+}
+
+
+var divCallback = function (req, res) {
+    var a = parseFloat(req.query.a);
+    var b = parseFloat(req.query.b);
+    var result  = a / b;
+
+    res.send(result.toString()); // send response body
+}
+
+app.get("/addTodo", addCallback);
+app.get("/sub", subCallback);
+app.get("/mult", multCallback);
+app.get("/div", divCallback);
+
+
+
+
+
+app.use(methodOverride());
+app.use(bodyParser());
+app.use(express.static(__dirname + '/public'));
+app.use(errorHandler());
+
+console.log("Simple static server listening at http://" + hostname + ":" + port);
+app.listen(port);
