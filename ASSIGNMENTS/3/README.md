b84e2d15987e08c51ea408672fefb3fa399a7a95
tejaswigowda <tejaswi@foxyninjastudios.com>
2017-02-12 23:16:33 -0700

    A3 posted

---
+++ ASSIGNMENTS/3/README.md
@@ -0,0 +1,14 @@
+#Assignment 3
+
+**Due Feb 20th before class**
+
+## Task
+
+Starting with the code handout write a server that can evaluate any
+numerical equation the user inputs.
+
+## Submisssion
+
+Email me the IP address as URL. e.g if my server IP was 12.56.134.53,
+then my submission URL would be **http://12.56.134.53/index.html**
+
---
+++ ASSIGNMENTS/3/code/server.js
@@ -0,0 +1,29 @@
+var express = require("express");
+var app = express();
+var bodyParser = require('body-parser');
+var errorHandler = require('errorhandler');
+var methodOverride = require('method-override');
+var hostname = process.env.HOSTNAME || 'localhost';
+var port = 8080;
+
+app.get("/", function (req, res) {
+      res.redirect("/index.html");
+});
+
+
+app.get("/eval", function (req, res) {
+
+    res.send(result.toString()); // send response body
+});
+
+
+
+
+
+app.use(methodOverride());
+app.use(bodyParser());
+app.use(express.static(__dirname + '/public'));
+app.use(errorHandler());
+
+console.log("Simple static server listening at http://" + hostname + ":" + port);
+app.listen(port);
