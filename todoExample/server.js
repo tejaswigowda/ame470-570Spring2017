696d1228c4b139966866dbd57e0887b5da252a0e
tejaswigowda <tejaswi@foxyninjastudios.com>
2017-03-01 09:59:27 -0700

    in class

--- todoExample/server.js
+++ todoExample/server.js
@@ -6,6 +6,8 @@ var methodOverride = require('method-override');
 var hostname = process.env.HOSTNAME || 'localhost';
 var port = 8080;
 
+var db = require('mongoskin').db('mongodb://user:pwd@127.0.0.1:27017/tododb');
+
 app.get("/", function (req, res) {
       res.redirect("/index.html");
 });
@@ -16,8 +18,18 @@ var todoList = [];
 
 
 app.get("/addTodo", function (req, res) {
-    todoList.push(req.query);
-    res.send(JSON.stringify(todoList));
+  db.collection("data").insert(req.query, function(err, result){
+      if(err){
+        res.send("error"); 
+      }
+      else{
+        db.collection("data").find({}).toArray( function(err1, result1) {
+          res.send(JSON.stringify(result1));
+        });
+      }
+  });
+   // todoList.push(req.query);
+   // res.send(JSON.stringify(todoList));
 });
 
 
@@ -28,7 +40,11 @@ app.get("/deleteTodo", function (req, res) {
 });
 
 app.get("/getTodos", function (req, res) {
-    res.send(JSON.stringify(todoList));
+  db.collection("data").find({}).toArray( function(err, result) {
+    res.send(JSON.stringify(result));
+  });
+
+   // res.send(JSON.stringify(todoList));
 });
 
 app.use(methodOverride());
