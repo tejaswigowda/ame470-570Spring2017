8223734ba7638785e23fe9559ab6d9398fbbf596
tejaswigowda <tejaswi@foxyninjastudios.com>
2017-03-01 10:13:28 -0700

    in class

--- todoExample/server.js
+++ todoExample/server.js
@@ -34,9 +34,19 @@ app.get("/addTodo", function (req, res) {
 
 
 app.get("/deleteTodo", function (req, res) {
-    var index = parseInt(req.query.index);
-    todoList.splice(index,1);
-    res.send(JSON.stringify(todoList));
+    var id = req.query.id.toString();
+   db.collection("data").remove({id: id}, function(err, result){
+      if(err){
+        res.send("error"); 
+      }
+      else{
+        db.collection("data").find({}).toArray( function(err1, result1) {
+          res.send(JSON.stringify(result1));
+        });
+      }
+   });
+   // todoList.splice(index,1);
+   // res.send(JSON.stringify(todoList));
 });
 
 app.get("/getTodos", function (req, res) {
